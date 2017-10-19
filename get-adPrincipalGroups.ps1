8ba648504afe5225cb963d71cd3f79632e2a13d1
Ben <benjaminriemersma@gmail.com>
2017-10-18 20:11:55 -0400

    removed silly fonts

--- get-adPrincipalGroups.ps1
+++ get-adPrincipalGroups.ps1
@@ -10,6 +10,6 @@ $payload.user1sADGroupNames=@()
 $payload.user2sADGroupNames=@()
 $payload.user1Name = $user1
 $payload.user2Name = $user2
-Get-ADPrincipalGroupMembership $user1 | Select-Object name | ForEach-Object{$payload.user1sADGroupNames+=$_.name}
-Get-ADPrincipalGroupMembership $user2 | Select-Object name | ForEach-Object{$payload.user2sADGroupNames+=$_.name}
+Get-ADPrincipalGroupMembership $user1 | select name | ForEach-Object{$payload.user1sADGroupNames+=$_.name}
+Get-ADPrincipalGroupMembership $user2 | select name | ForEach-Object{$payload.user2sADGroupNames+=$_.name}
 $payload  | ConvertTo-Json | Out-Host
