0e738eb66a84fc743f8807e97586b1fe9f8b644f
Ben <benjaminriemersma@gmail.com>
2018-02-25 18:40:23 -0500

    consolidate my module files

--- my_modules/myActions.js
+++ my_modules/myActions.js
@@ -5,7 +5,192 @@ const {Set} = require('immutable');
 const DOM = require('./DOMmanipulation.js');
 var Queue = require('better-queue');
 
-module.exports.COMPARE = (outputfromPS, names) => {
+
+module.exports.compareBtnClickedUpdateDOM = () => {
+    $('.mainForm').addClass("disabled");
+    $('#redMessageBar').empty();
+    setTimeout(function(){
+        $('#queryingSign').removeClass('hidden');
+        $('#userinputarea').slideToggle("slow");
+        $('#emptyRow').html(`<div class="row center">
+                                <div class="progress">
+                                    <div class="indeterminate"></div>
+                                </div>  
+                            </div>`);},500);
+    };
+
+module.exports.removeBtnClickedUpdateDOM = () => {
+    $('#removeGroupsInput').addClass("disabled");
+    $('#redMessageBar').empty();
+    setTimeout(function(){
+        $('#queryingSignRemoveTab').removeClass('hidden');
+        $('#removeuserinputarea').slideToggle("slow");
+        $('#emptyRow').html(`<div class="row center">
+                                <div class="progress">
+                                    <div class="indeterminate"></div>
+                                </div>  
+                            </div>`);},500);
+    };
+
+
+module.exports.validateMySingleUser = (u1, userName) => {
+    function _resetMyForm(){
+        $('#removeGroupsInput').removeClass("disabled");
+        $('#removeuserinputarea').slideToggle("slow");
+        $('#emptyRow').empty();
+        $('#btnRemove').addClass('ready');
+        $('#queryingSignRemoveTab').slideToggle('slow');
+    }
+
+    if(u1 ===""){
+        setTimeout(function(){
+            _resetMyForm();
+            $('#redMessageBar').html(`You must enter a uniqname`);
+        },1000);  //if you dont wait for 1000 its too fast for the other animations
+        return; //end the function before we even begin our PS process if theres a blank input field
+    }
+    let ps = new powershell({
+        executionPolicy: 'Bypass',
+        noProfile: true
+    });
+    ps.addCommand(`./get-ADUser -u1 ${u1}`);
+    ps.invoke()
+    .then(output=>{
+        ps.dispose();
+        const data=JSON.parse(output);
+        if(data[3].Value.ModuleFound ===false){$('#redMessageBar').html(`This program cannot check your effectice permissions without PowerShell Access Control Module.  Please reinstall the program as administrator.  You can download it from the internet and unzip it to C:\\Program Files\\WindowsPowerShell\\Modules but you will still need local admin to do that.`);return;};
+        if(data[2].Error.Message !== "No error"){
+           _resetMyForm(); //error occurred, reset form
+           $('#redMessageBar').html(data[2].Error.Message); //report the error
+           return; //end the function
+        }else{
+            
+            let names= {
+                'user1Name' : u1,
+                'user1DN' : (data[0].DN).toString(),
+                'currentUser' : userName
+            };
+            //animation needed
+            //$('#queryingSignRemoveTab').slideToggle('slow');
+            
+            return listOfGroupsToRemove(names);
+        }
+    })
+    .catch(err=>{
+        ps.dispose();
+    });
+};
+
+module.exports.validateMyList = (u1, u2, userName) => {
+
+    function _resetMyForm(){ //DOM manipulation for when the process is cancelled
+        $('.mainForm').removeClass("disabled");
+        $('#userinputarea').slideToggle("slow");
+        $('#emptyRow').empty();
+        $('#btnCompare').addClass('ready');
+    }
+
+    if (u1 ==="" || u2===""){
+        setTimeout(function(){
+            _resetMyForm();
+            $('#redMessageBar').html(`You must enter 2 uniqnames`);
+        },1000);  //if you dont wait for 1000 its too fast for the other animations
+        return; //end the function before we even begin our PS process if theres a blank input field
+    }
+
+    let ps = new powershell({
+        executionPolicy: 'Bypass',
+        noProfile: true
+    });
+    ps.addCommand('./get-ADUser',[{u1:`"${u1}"`},{u2:`"${u2}"`}]);
+    ps.invoke()
+    .then(output=>{
+        ps.dispose();
+        const data=JSON.parse(output);
+        if(data[3].Value.ModuleFound ===false){$('#redMessageBar').html(`This program cannot check your effectice permissions without PowerShell Access Control Module.  Please reinstall the program as administrator.  You can download it from the internet and unzip it to C:\\Program Files\\WindowsPowerShell\\Modules but you will still need local admin to do that.`);return;};
+        if(data[2].Error.Message !== "No error"){
+           _resetMyForm(); //error occurred, reset form
+           $('#redMessageBar').html(data[2].Error.Message); //report the error
+           return; //end the function
+        }else{
+                let names= {
+                    'user1Name' : u1,
+                    'user2Name' : u2,
+                    'user1DN' : (data[0].DN).toString(),
+                    'user2DN' : (data[1].DN).toString(),
+                    'currentUser' : userName
+                };
+                $('#user1').append(`<h4 class="wildwestfontStriped brown-text text-darken-3">${names.user1Name}</h4>`);
+                $('#user2').append(`<h4 class="wildwestfontStriped brown-text text-darken-3" id="user2sName">${names.user2Name}</h4><ul class="blue darken-1"><span class="amber-text text-lighten-1">`);
+                $('#queryingSign').text(`Checking for ${u1} and ${u2}'s memberships`);
+                    return listOfGroupsToCompare(names);
+                     }
+    })
+    .catch(err=>{
+        ps.dispose();
+    });    
+};
+
+
+
+/*
+FUNCTION: listOfGroupsToCompare
+ @param {JSON array element} names:
+ {String} u1DN distinguished name of "user 1"
+ {String} u2DN distinguished name of "user 2"
+ {String} u1Name short name of "user 1"
+ {String} u2Name short name of "user 2"
+ {String} currentUserName short name of the user running this program
+
+ Description: Updates the DOM with a list of both matching and nonmatching group memberships.
+ Then it checks User 1's nonmatching groups to see if the current user has permission to add
+ User 2 to any of the groups and it updates the DOM accordingly.
+
+PowerShell scripts:
+get-adPrincipalGroups to build 2 lists of group memberships to compare
+get-effective-access to see if you can add user 2 to any of user 1's groups
+add-adgroupmember to add user 2 to user 1's groups 1 at a time
+remove-adGroupMember to undo after a group has been added
+
+*/
+
+const listOfGroupsToCompare = (names) => {
+    let ps = new powershell({
+        executionPolicy: 'Bypass',
+        noProfile: true
+    }); 
+    ps.addCommand(`./get-adPrincipalGroups.ps1 -user1 '${names.user1DN}' -user2 '${names.user2DN}'`);
+    ps.invoke()
+    .then(output =>  {
+        ps.dispose();
+        COMPARE(output, names);
+    })
+    .catch(err=>{
+        $('#redMessageBar').html(err);
+        ps.dispose();
+    });
+};
+
+const listOfGroupsToRemove = (names) => {
+    let ps = new powershell({
+        executionPolicy: 'Bypass',
+        noProfile: true
+    }); 
+    ps.addCommand(`./get-adPrincipalGroups.ps1 -user1 '${names.user1DN}'`);
+    ps.invoke()
+    .then(output =>  {
+        ps.dispose();
+        REMOVE(output, names);
+    })
+    .catch(err=>{
+        $('#redMessageBar').html(err);
+        ps.dispose();
+    });
+};
+
+//compare and remove functions
+
+const COMPARE = (outputfromPS, names) => {
 
     const _removeADGroup = (groupDN, user2, i, cb) => {
         let psxAsync = new powershell({
@@ -131,7 +316,7 @@ module.exports.COMPARE = (outputfromPS, names) => {
 
 
 
-module.exports.REMOVE = (outputfromPS, names) => {
+const REMOVE = (outputfromPS, names) => {
 
     const _readdGroup = (groupDN, i, cb) => {
         let psReadd = new powershell({
--- renderer.js
+++ renderer.js
@@ -24,10 +24,10 @@ const powershell = require('node-powershell');
 window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.js'); // I use this when the NPM 'node_modules' folder is in my project and NOT installed globally.
 window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js'); // The sequence of these requires is important
 require('materialize-css');
-const myModules = require('./my_modules/validate-userNames.js');
+const myModules = require('./my_modules/myActions.js');
 
 $(document).ready(() => {
-    $('.modal').modal();
+    $('.modal').modal(); //initialize Materialize modal
     $(".tabs>li>a").css("text-color", '#FFFFFF'); //a funky fix for Materialize's sass to make the tabs font color white
     $('.tooltipped').tooltip({delay: 50}); //initialize tooltips
     $('#yourNameHere').html(userName); //update DOM with current users name
