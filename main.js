b28d3d9005e6317460404d565905c57da60e2954
Ben <benjaminriemersma@gmail.com>
2018-05-02 21:45:00 -0400

    speed enhancements

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 "use strict";
 
+var lru = require('lru-cache')({max: 256, maxAge: 250/*ms*/});
+
+var fs = require('fs');
+var origLstat = fs.lstatSync.bind(fs);
+
+// NB: The biggest offender of thrashing lstatSync is the node module system
+// itself, which we can't get into via any sane means.
+require('fs').lstatSync = function(p) {
+  let r = lru.get(p);
+  if (r) return r;
+
+  r = origLstat(p);
+  lru.set(p, r);
+  return r;
+};
+
+
 const electron = require('electron');
 //const app = electron.app
 //destructured into...
--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
+var lru = require('lru-cache')({max: 256, maxAge: 250/*ms*/});
+
+var fs = require('fs');
+var origLstat = fs.lstatSync.bind(fs);
+
+// NB: The biggest offender of thrashing lstatSync is the node module system
+// itself, which we can't get into via any sane means.
+require('fs').lstatSync = function(p) {
+  let r = lru.get(p);
+  if (r) return r;
+
+  r = origLstat(p);
+  lru.set(p, r);
+  return r;
+};
+
+
 window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.js'); // I use this when the NPM 'node_modules' folder is in my project and NOT installed globally.
 window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js'); // The sequence of these requires is important
 require('materialize-css');
